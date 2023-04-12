b210cf4198455c4bf652e5a0f4ff3406a282db98
GRA <smpln-41c34b01292@naturalobjects.fr>
2023-04-13 00:37:48 +0200

    [tweak] fun typo :) !

--- header.php
+++ header.php
@@ -27,7 +27,7 @@ if ($_COOKIE['prefer-inverted-theme']) {
             <ul>
                 <li><a href="./hello-girls.php">Hello Girls !</a></li>
                 <li><a href="./hello-boys.php">Hello Boys !</a></li>
-                <li><a href="./secret.php">Secret !...</a></li>
+                <li><a href="./secret.php">Secret !?..</a></li>
                 <li>
                     <form action="./invert-theme.php">
                         <button type="submit">Invert theme</button>
