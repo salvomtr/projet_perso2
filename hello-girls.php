4a610765b6af6256b2cdcd8d424a23b7b369288a
GRA <smpln-41c34b01292@naturalobjects.fr>
2023-04-12 22:02:42 +0200

    fait en sorte que chacune des pages utilise le thème préféré enregistré pour l'Utilisateur.

--- hello-girls.php
+++ hello-girls.php
@@ -1,3 +1,12 @@
+<?php
+
+$theme = 'style';
+if ($_COOKIE['prefer-inverted-theme']) {
+    $theme .= '-inverted';
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -5,7 +14,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="./css/<?= $theme ?>.css">
     <title>Hello Girls !</title>
 </head>
 
