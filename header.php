c38eb9e803056a2d5e3eddc1759511e430689a0e
GRA <smpln-41c34b01292@naturalobjects.fr>
2023-04-12 21:54:31 +0200

    Ecrit 2 pages php quelconques avec un minimum de contenu, et proposer un bandeau de navigation 'header.php' pour passer de l'une à l'autre (en utilisant 'include' pour ne pas se répêter).

---
+++ header.php
@@ -0,0 +1,8 @@
+<header>
+    <nav>
+        <ul>
+            <li><a href="./hello-girls.php">Hello Girls !</a></li>
+            <li><a href="./hello-boys.php">Hello Boys !</a></li>
+        </ul>
+    </nav>
+</header>
---
+++ hello-girls.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>Hello Girls !</title>
+</head>
+
+<body>
+    <?php include './header.php' ?>
+
+    <h1>Hello Girls !</h1>
+</body>
+
+</html>
