ff4ebe4ea2b9e4efe9e20f4ee1acec1de08af06d
GRA <smpln-41c34b01292@naturalobjects.fr>
2023-04-13 05:30:23 +0200

    [tweak] Encore du découpage, à pas cher.

--- view/partial/header.php
+++ view/partial/header.php
@@ -16,7 +16,7 @@ if ($_COOKIE['prefer-inverted-theme']) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/css/<?= $theme ?>.css">
+    <link rel="stylesheet" href="/asset/css/<?= $theme ?>.css">
     <title><?= $pageTitle ?></title>
 </head>
 
