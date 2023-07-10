cebf132ee9e6dacfc8ff351f0bbbb837b7c74d18
SMA <salvomtr@outlook.com>
2023-07-11 00:38:54 +0200

    creation db

---
+++ DB/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO `role` (`id`, `nomRole`) VALUES (NULL, 'gestionnaire'), (NULL, 'utilisateur');
+
+INSERT INTO `utilisateur` (`id`, `nom`, `prenom`, `mail`, `motDePasse`, `idRole`) VALUES (NULL, 'Matraxia', 'Salvatore', 'salvomtr@outlook.com', '123456', '1');
+INSERT INTO `utilisateur` (`id`, `nom`, `prenom`, `mail`, `motDePasse`, `idRole`) VALUES (NULL, 'Todorov', 'Ion', 'todorov@outlook.com', '222222', '2');
+
+INSERT INTO `article` (`id`, `titre`, `descriptionCourte`, `textArticle`, `immage`, `idUtilisateur`) VALUES (NULL, 'Salade César', 'The famous César Salade', 'Salade, poulet, parmesan, crouton de pain, jambon', '', '1');
